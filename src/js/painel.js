723ab5d48b1c5974efb93bfd553776c74053da21
Héric Gomes da Silva <h1g2s0@hotmail.com>
2023-06-27 20:15:30 -0300

    Iniciando JS
    Iniciando JS

---
+++ src/js/painel.js
@@ -0,0 +1,15 @@
+/*
+Quando clicar na seta para avançar temos que esconder todas as imagens
+e mostrar a prócima imagem.
+
+A imagem atual começa em Zero(0) pq é a primeira imagem, assim que for
+clicado no avançar eu preciso adicionar mais 1 ao contador de imagens
+para poder saber que agora eu vou mostrar a segunda imagem.
+
+    Esconder todas as imagens
+        pegar todas as imagens usando o DOM e remover a classe mostrar
+
+    Mostrar a próxima imagem
+        pegar todas as imagens, descobrir qual é a próxima, e colocar
+        a classe mostrar nela
+*/
